4b6305d1f0cffd602d5032fa431a9c97481dfe31
NOMAN ARSHAD <NOMAN ARSHAD@DESKTOP-62PMHN6>
2022-02-12 22:39:06 +0500

    ok

--- New_Java/src/New_Java/Print.java
+++ New_Java/src/New_Java/Print.java
@@ -3,5 +3,14 @@ public class Print{
 
 	public static void main() throws Exception{
 		System.out.println("First project to push at GitHub");
+		System.out.println("First project to push at GitHub");
+		System.out.println("First project to push at GitHub");
+		System.out.println("First to push at GitHub");
+		go();
+	}
+
+	private static void go() {
+		// TODO Auto-generated method stub
+		
 	}
 }
